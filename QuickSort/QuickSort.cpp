f589f1dc325a15424d9373b2e9e4fbddd00ad8a4
Latfiansya <latfiansyadzikri@gmail.com>
2024-03-28 11:27:04 +0700

    step 9 menukar elemen pada index i dengan elemen pada index j

--- QuickSort/QuickSort.cpp
+++ QuickSort/QuickSort.cpp
@@ -66,4 +66,11 @@ void q_short(int low, int high)
 			j--; // step 8
 			cmp_count++;
 		}
+		cmp_count++;
+		if (i < j) // step 9
+		{
+			// menukar elemen pada index i dengan elemen pada index j
+			swap(i, j);
+		}
 	}
+}
