9963690ea41e84c86d2f85d646e56bd429a351ee
Latfiansya <latfiansyadzikri@gmail.com>
2024-03-28 11:18:00 +0700

    Prosedur Input

--- QuickSort/QuickSort.cpp
+++ QuickSort/QuickSort.cpp
@@ -5,3 +5,27 @@ int arr[20];
 int cmp_count = 0; // angka yang dibandingkan (comparasion)
 int mov_count = 0; // angka yang dipindah (movements)
 int n;
+
+void input()
+{
+	while (true)
+	{
+		cout << "Masukkan panjang elemen array : ";
+		cin >> n;
+
+		if (n <= 20)
+			break;
+		else
+			cout << "\nMaksimum panjang array adalah 20\n";
+	}
+
+	cout << "\n---------------------\n";
+	cout << "Masukkan Elemen Array\n";
+	cout << "---------------------\n";
+
+	for (int i = 0; i < n; i++)
+	{
+		cout << "<" << (i + 1) << "> ";
+		cin >> arr[i];
+	}
+}
